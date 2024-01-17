f267c4f174fe005b51432e7d8b903cbe80ff1a8e
monjurur <monjurur.roshid@efgt.eurofoods-bd.com>
2024-01-17 16:51:35 +0600

    update 17/01/24

--- lib/pages/postList.dart
+++ lib/pages/postList.dart
@@ -14,37 +14,36 @@ class PostListPage extends StatefulWidget {
   State<PostListPage> createState() => _PostListPageState();
 }
 
-
 class _PostListPageState extends State<PostListPage> {
   final url = "http://118.179.81.91:80/personnel/api/employees/";
 
   var _postJson = [];
-List<EmployeeModel> employeeList = [];
-bool  isLoading = false;
+  List<EmployeeModel> employeeList = [];
+  bool isLoading = false;
+  int nextPage = 1;
+  int pageSize = 0;
   List<EmployeeModel> employees = [];
 
- Future<void>  fetchPosts() async {
+  Future<void> fetchPosts() async {
     try {
       final response = await get(Uri.parse(url));
 
       final jsonData = jsonDecode(response.body) as List;
-      if(jsonData.isNotEmpty){
+      if (jsonData.isNotEmpty) {
         setState(() {
           _postJson = jsonData;
           isLoading = true;
         });
       }
-
-
-    }catch(e){
+    } catch (e) {
       print("Error :$e");
     }
   }
 
-
   Future<void> fetchData() async {
     // Define the API endpoint URL
-    String apiUrl = "http://118.179.81.91:80/personnel/api/employees/";
+    int pp =1;
+    String apiUrl = "http://118.179.81.91:80/personnel/api/employees/?page=3";
 
     // Define your Basic Authentication credentials
     String username = "admin";
@@ -66,46 +65,89 @@ bool  isLoading = false;
 
     // Check the status code to see if the request was successful
     if (response.statusCode == 200) {
+pp++;
       // Parse the response data (assuming it's JSON)
       final Map<String, dynamic> data = json.decode(response.body);
+
       setState(() {
-        EmployeeResponse employeeResponse =
-        EmployeeResponse.fromJson(data);
+        isLoading = true;
+        EmployeeResponse employeeResponse = EmployeeResponse.fromJson(data);
         employees = employeeResponse.data;
-     employees..forEach((element) {
-          print(element.id);
-        });
+        employeeList.addAll(employeeResponse.data);
+        for (nextPage; nextPage <= 7; nextPage++) {
+
+        pp++;
+        print(nextPage);
+       // fetchData();
+      }
+
+        print(employeeList);
+        employees
+          ..forEach((element) {
+            print(element.id);
+          });
       });
-
-
     } else {
       // Handle the error
       print("Error: ${response.statusCode} - ${response.reasonPhrase}");
     }
   }
 
-
   @override
-  void initState()  {
+  void initState() {
     // TODO: implement initState
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
       // await checkTokenExpiration();
-      await  fetchData();
+      await fetchData();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-     Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: const Text("Post List"),
-          ),
-          body:!isLoading ? const Center(
-            child: CircularProgressIndicator(),
-          ): ListView.builder(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Post List"),
+      ),
+      body: !isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          :
+          /*.employees.forEach((element) {
+            print(element.id);
+          });*/
+          /*SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Dynamic Items:'),
+                // Use a for loop to create widgets dynamically inside a Column
+                for (var item in employees)
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text(
+                          "${item.fullName}",
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                        subtitle: Text("${item.department.deptName}"),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),*/
+
+      ListView.builder(
               padding: const EdgeInsets.all(8.0),
               itemCount: employeeList.length,
               itemBuilder: (context, index) {
@@ -121,14 +163,13 @@ bool  isLoading = false;
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: ListTile(
-                      title:Text("${employeeList}",style: TextStyle(fontWeight: FontWeight.w600),) ,
+                      title:Text("${employeeList[index].fullName}",style: TextStyle(fontWeight: FontWeight.w600),) ,
                      // subtitle: Text("${post["body"]}"),
                     ),
                   ),
                 );
               }
           )
-      );
-
+    );
   }
 }
